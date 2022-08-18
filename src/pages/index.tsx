c2c55e751f5beedb4d5d4059b874e8aad3fba0d1
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-08-18 18:40:03 +0900

    subsc-error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
+/* eslint-disable no-shadow */
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import useStore from "../store";
+import { supabase } from "../utils/supabase";
+import { Layout } from "../components/Layout";
+import { Auth } from "../components/Auth";
+import { DashBoard } from "../components/DashBoard";
 
-const Home: NextPage = () => <div />;
+const Home: NextPage = () => {
+  const session = useStore((state) => state.session);
+  const setSession = useStore((state) => state.setSession);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log(session);
+      console.log(supabase.auth.user());
+
+      setSession(session);
+    });
+  }, [setSession]);
+
+  return (
+    <Layout title="Dashboard">{!session ? <Auth /> : <DashBoard />}</Layout>
+  );
+};
 
 export default Home;
